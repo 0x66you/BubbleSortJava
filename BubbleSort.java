3a7fb2990036d1f96d85c09d7ffce1a9c38b3d68
YehChunChen_ <91467203+0x66you@users.noreply.github.com>
2021-11-26 22:41:42 +0800

    Add files via upload

---
+++ BubbleSort.java
@@ -0,0 +1,31 @@
+/*
+ *Copyright (c) 2021 Chun-Chen,Yeh - All Rights Reserved 
+*/
+package chap7;
+import org.junit.jupiter.api.*;
+/**
+ *  @author   ChunChenYeh
+ *  開發日期:  Nov 26, 2021
+*/
+public class BubbleSort {
+	@Test
+	public void bubblesort() {
+		int[] data = {8,7,6,5,4,3,2,1}; 
+		int temp,k=0;
+		for(int i = data.length-1;i>0;i--) {
+			for(int j = i;j>0;j--) {
+				if(data[k]>data[k+1]) {
+					temp = data[k];
+					data[k] = data[k+1];
+					data[k+1] = temp;
+				}
+				k++;	
+			}
+			k=0;
+			for(int n:data) {
+				System.out.print(n+" ");
+			}
+			System.out.println();
+		}
+	}
+}
